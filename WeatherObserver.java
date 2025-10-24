10c5c7554570d2971bfdd4c1c4caf4656b61dbd5
GithubNeek <161673122+GithubNeek@users.noreply.github.com>
2025-10-24 20:58:42 +1100

    Update WeatherObserver.java

--- WeatherObserver.java
+++ WeatherObserver.java
@@ -1,19 +1,12 @@
-// WeatherObserver.java (FINAL UPDATE)
 
 import java.util.List;
 
-/**
- * Observer interface for components that react to weather updates.
- */
+
 public interface WeatherObserver {
     
-    // The Assignment 2 method: handles continuous numerical data
     public void updateWeather(List<WeatherDataPoint> weatherData);
     
-    // The Legacy method (for the SUNNY/RAIN/SNOW enum)
-    // We use a default implementation so classes implementing the new method 
-    // don't break if they don't care about the old enum events.
+    
     public default void updateLegacyWeather(WeatherEvent event) {
-        // Default does nothing
     }
 }
